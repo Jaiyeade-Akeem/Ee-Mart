5bde5f26ab567e0042c3493cee8fdf8429edd7db
jaiyeadeakeem <akeem.jaiyeade@decagon.dev>
2023-03-16 08:42:20 +0100

    Ea-Mart

---
+++ src/main/java/com/example/eemart/EeMartApplication.java
@@ -0,0 +1,13 @@
+package com.example.eemart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EeMartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EeMartApplication.class, args);
+    }
+
+}
